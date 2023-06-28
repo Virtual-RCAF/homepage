8ba38397583a93f9af4d6c7471445160e35f118e
Evan Anderson <e.e.anderson@shaw.ca>
2023-06-28 15:29:58 -0600

    remove build

--- out/_next/static/50DHJe8B_dc_NrAGFUTvt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5f6b35040556afc4.css","static/chunks/pages/index-28265c84006ac36d.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
